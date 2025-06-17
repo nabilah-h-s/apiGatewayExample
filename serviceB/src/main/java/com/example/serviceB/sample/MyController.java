02be3524bd11c3fe7843a62525562d8384d1ec0d
nabilah-H-S <haliban256@gmail.com>
2025-06-17 09:00:58 +0600

    initializing service B

---
+++ serviceB/src/main/java/com/example/serviceB/sample/MyController.java
@@ -0,0 +1,15 @@
+package com.example.serviceB.sample;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	@GetMapping("/home")
+	public ResponseEntity<?> getHomeInfo(){
+		return ResponseEntity.ok("Hello from ServiceB");
+	}
+
+}
