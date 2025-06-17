7bc2d940e2a15847d3876dce25a7ff1e5e205ea2
nabilah-H-S <haliban256@gmail.com>
2025-06-17 23:41:04 +0600

    Filter- Built in filter (AddRequestHeader)

--- myGateway/myGateway/src/main/java/com/example/myGateway/configuration/CustomGlobalFilter.java
+++ myGateway/myGateway/src/main/java/com/example/myGateway/configuration/CustomGlobalFilter.java
@@ -16,6 +16,10 @@ public class CustomGlobalFilter implements GlobalFilter {
         System.out.println("GlobalFilter - Pre-processing");
         System.out.println("Request URI: " + exchange.getRequest().getURI());
         
+        exchange.getRequest().getHeaders().forEach((key, value) ->
+            System.out.println("Request Header: " + key + " = " + value)
+        );
+
         return chain.filter(exchange).then(
             Mono.fromRunnable(() -> {
                 // ✅ Post-processing: Log response headers and status
